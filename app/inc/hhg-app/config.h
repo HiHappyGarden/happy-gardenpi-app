a7111cf9d03e43c42dbe3d52c79f1e05e0c5459f
Antonio Salsi <passy.linux@zresa.it>
2023-07-08 14:42:11 +0200

    feat: project refactoring

---
+++ app/inc/hhg-app/config.h
@@ -0,0 +1,29 @@
+/***************************************************************************
+ *
+ * Hi Happy Garden Interfaces
+ * Copyright (C) 2023  Antonio Salsi <passy.linux@zresa.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+#ifndef HHGARDEN_APP_CONFIG_H
+#define HHGARDEN_APP_CONFIG_H
+
+#define HHGARDEN_APP_NAME "hhg-app"
+#define HHGARDEN_APP_VER "0.52.0"
+#define HHGARDEN_APP_VER_MAJOR 0
+#define HHGARDEN_APP_VER_MINOR 52
+#define HHGARDEN_APP_VER_PATCH 0
+
+#endif // HHGARDEN_APP_CONFIG_H
