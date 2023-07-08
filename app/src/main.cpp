3bb19973ce94937f0fd607686a354425bf280a41
Antonio Salsi <passy.linux@zresa.it>
2023-07-08 05:43:43 +0200

    feat/start new project

---
+++ app/src/main.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *
+ * PROJECT
+ * Copyright (C) 202X  Antonio Salsi <passy.linux@zresa.it>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ***************************************************************************/
+
+
+#include <stdio.h>
+
+namespace hhgarden
+{
+inline namespace v1
+{
+
+}
+}
+
+
+void pippo()
+{
+    throw 1;
+}
+
+int main(int argc, char* argv[]) try
+{
+    pippo();
+}
+catch(const int& e)
+{
+    printf("error %d\n", e);
+}
