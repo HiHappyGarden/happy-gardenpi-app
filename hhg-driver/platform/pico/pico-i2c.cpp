cb7f777c9519c961eee8b8f28b096bf93f39cba6
Antonio Salsi <passy.linux@zresa.it>
2024-04-22 22:29:01 +0200

    fix: i2c driver

--- hhg-driver/platform/pico/pico-i2c.cpp
+++ hhg-driver/platform/pico/pico-i2c.cpp
@@ -52,8 +52,7 @@ os::exit pico_i2c::init(error** error) OS_NOEXCEPT
 	singleton = this;
 
     i2c_init(PICO_DEFAULT_I2C_INSTANCE, SPEED);
-
-    bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN, PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C));
+    bi_decl(bi_2pins_with_func(pico_i2c::pin::SDA_PIN, pico_i2c::pin::SCL_PIN, GPIO_FUNC_I2C));
 
 	return exit::OK;
 }
