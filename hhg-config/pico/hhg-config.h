16b63fefd5bc1c84879a5f9a1a7bf0896173deb8
Antonio Salsi <passy.linux@zresa.it>
2024-05-07 00:10:37 +0200

    feat: continue develop fon handler

--- hhg-config/pico/hhg-config.h
+++ hhg-config/pico/hhg-config.h
@@ -21,9 +21,9 @@
 #define HHG_CONFIG_H
 
 #define HHG_NAME "hi-happy-garden-app"
-#define HHG_VER "0.50.0"
+#define HHG_VER "0.60.0"
 #define HHG_VER_MAJOR (0)
-#define HHG_VER_MINOR (50)
+#define HHG_VER_MINOR (60)
 #define HHG_VER_PATCH (0)
 
 #define HHG_SCHEDULES_SIZE (2)
