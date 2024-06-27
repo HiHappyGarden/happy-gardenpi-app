360e339d16f22c6ccf73bb9a046827f945c2f424
Antonio Salsi <passy.linux@zresa.it>
2024-06-27 13:40:09 +0200

    feat: continue to develop menu and start develop keyboard

--- inc/asset/ic_wifi_on.hpp
+++ inc/asset/ic_wifi_on.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *
  * Hi Happy Garden
- * Copyright (C) 2023-2024  Antonio Salsi <passy.linux@zresa.it>
+ * Copyright (C) 2023/2024 Antonio Salsi <passy.linux@zresa.it>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,17 +18,29 @@
  ***************************************************************************/
 
 #pragma once
-namespace hhg {
-    inline namespace v1 {
 
-        namespace {
-            uint8_t font_test[]
-                    {
-                            0x0C, 0x10, // font width, height
-                            0xFF, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x00, 0x00, 0x00, 0x00,         //row0 = 12 pix x 8 pix
-                            0xFF, 0x87, 0x85, 0x87, 0x80, 0x80, 0x80, 0x80, 0x41, 0x22, 0x14, 0x08   //row1 = 12 pix x 8 pix
-                    };
+namespace hhg
+{
+inline namespace v1
+{
 
-        }
-    }
+namespace
+{
+constexpr uint8_t ic_wifi_on_width = 12;
+constexpr uint8_t ic_wifi_on_height = 10;
+constexpr uint8_t ic_wifi_on[] = {
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+        0, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0,
+        1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1,
+        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+        0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+        0, 0, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0
+};
+}
+
+}
 }
